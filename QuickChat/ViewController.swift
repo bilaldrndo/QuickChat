e5a958347ddd5c4c4cf2f20cf1a268fb73633f0f
bilaldrndo <drndob2003@gmail.com>
2018-07-20 15:35:51 +0200

    Initial Commit

---
+++ QuickChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by Bilal on 7/20/18.
+//  Copyright © 2018 Bilal Drndo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
